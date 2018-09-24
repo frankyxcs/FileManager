c3c05947133f953a453add7e5e35cce5373c7eac
tadeas-braun <tadeas.braun@gmail.com>
2018-09-24 18:00:11 +0200

    try to open file with default application

--- app/src/main/java/cz/brauntadeas/filemanager/FileAdapter.java
+++ app/src/main/java/cz/brauntadeas/filemanager/FileAdapter.java
@@ -1,12 +1,21 @@
 package cz.brauntadeas.filemanager;
 
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -44,7 +53,18 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder
                 if (file.isDirectory()) {
                     updateList(file);
                 } else {
-
+                    Intent intent = new Intent();
+                    Context context = holder.itemView.getContext();
+                    Uri uri = getFileUri(file, context);
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    intent.setDataAndType(uri, getMimeType(uri, context));
+                    try {
+                        context.startActivity(intent);
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                        Toast.makeText(context, R.string.no_activity, Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
@@ -57,6 +77,28 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder
 
     private int getFileIcon(boolean isDirectory) {
         return isDirectory ? R.drawable.folder : R.drawable.file_outline;
+    }
+
+    private String getMimeType(Uri uri, Context context) {
+        String mimeType;
+        if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            ContentResolver contentResolver = context.getContentResolver();
+            mimeType = contentResolver.getType(uri);
+        } else {
+            String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
+        }
+        return mimeType;
+    }
+
+    private Uri getFileUri(File file, Context context) {
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", file);
+        } else {
+            uri = Uri.fromFile(file);
+        }
+        return uri;
     }
 
     class FileViewHolder extends RecyclerView.ViewHolder {
